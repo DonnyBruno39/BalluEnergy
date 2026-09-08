8ddc9c3a9ec30f88548bf209f35bf31b3b1a2067
DonnyBruno39 <8g5d4s4gcr@privaterelay.appleid.com>
2026-09-08 23:08:28 +0300

    Bump Ballu Energy to v0.3.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ru.balluenergy.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.3.6"
+        versionCode = 8
+        versionName = "0.3.7"
     }
 
     compileOptions {
@@ -37,5 +37,4 @@ dependencies {
     implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.google.code.gson:gson:2.11.0")
-    implementation("com.google.android.gms:play-services-auth:21.3.0")
 }
