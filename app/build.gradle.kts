d2a44c40d8fa1e9dc010bc4000ea10dddaaa133b
DonnyBruno39 <8g5d4s4gcr@privaterelay.appleid.com>
2026-09-08 22:42:10 +0300

    Bump Ballu Energy to v0.3.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ru.balluenergy.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.3.4"
+        versionCode = 6
+        versionName = "0.3.5"
     }
 
     compileOptions {
